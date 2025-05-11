9aaa20ca4971793fca57ec2885ba7c8a14387578
wangjue <348748267@qq.com>
2025-05-11 13:26:15 +0800

    feat: 新增小红书平台

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,6 @@ export const templateDir = 'template';
 export const choices: Choice[] = [
   { value: 'wechat', title: '微信小程序' },
   { value: 'toutiao', title: '头条小程序' },
-  { value: 'xhs', title: '小红书小程序' },
   { value: 'ali', title: '阿里小程序' },
   { value: 'one', title: '跨平台小程序' },
 ];
